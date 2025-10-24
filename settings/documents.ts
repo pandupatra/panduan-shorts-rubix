468cb24fc3cb65c2145f35da937dc5759cb53c56
pandupatra <pandamon56@gmail.com>
2025-10-24 22:15:37 +0800

    updated for production

--- settings/documents.ts
+++ settings/documents.ts
@@ -19,128 +19,143 @@ export const Documents: Paths[] = [
       }
     ]
   },
+  { spacer: true },
   {
     title: "Peran Video Pendek",
     href: "/bab-2",
     items: [
       {
-        title: "Definisi dan Karakteristik utama dari Shorts",
-        href: ""
+        title: "Mengelola Beban Kognitif Siswa",
+        href: "/teori-beban-kognitif"
+      },
+      {
+        title: "Microlearning",
+        href: "/microlearning"
+      },
+      {
+        title: "Keterlibatan dan Retensi",
+        href: "/keterlibatan-dan-retensi"
       }
     ]
   },
   {spacer: true},
   {
-    title: "Tahap Pra-Produksi",
-    heading: "Produksi Video Pendek",
-    href: "/bab-3/tahap-pra-produksi"
+    title: "Produksi Video Pendek",
+    href: "/bab-3",
+    items: [
+      {
+        title: "Tahap Pra-Produksi",
+        href: "/tahap-pra-produksi"
+      },
+      {
+        title: "Tahap Produksi",
+        href: "/tahap-produksi"
+      },
+      {
+        title: "Tahap Pasca-Produksi (Editing)",
+        href: "/tahap-pasca-produksi"
+      },
+      
+    ]
   },
-  {
-    title: "Tahap Produksi",
-    href: "/bab-3/tahap-produksi"
-  },
-  {
-    title: "Tahap Pasca-Produksi (Editing)",
-    href: "/bab-3/tahap-pasca-produksi"
-  },
-  {spacer: true},
+  { spacer: true },
   {
     title: "Galeri Inspirasi",
     href: ""
-  },
-  {
-    heading: "Introduction",
-    title: "Basic Setup",
-    href: "/basic-setup",
-    items: [
-      {
-        title: "Installation",
-        href: "/installation",
-      },
-      {
-        title: "Setup",
-        href: "/setup",
-      },
-      {
-        title: "Changelog",
-        href: "/changelog",
-      },
-    ],
-  },
-  {
-    spacer: true,
-  },
-  {
-    title: "Navigation",
-    href: "/navigation",
-    heading: "Documents",
-  },
-  {
-    title: "Structure",
-    href: "/structure",
-    items: [
-      {
-        title: "Deep",
-        href: "/deep",
-        items: [
-          {
-            title: "Deeper",
-            href: "/deeper",
-            items: [
-              {
-                title: "Even deeper",
-                href: "/even-deeper",
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    spacer: true,
-  },
-  {
-    title: "Markdown",
-    href: "/markdown",
-    heading: "Components",
-    items: [
-      {
-        title: "Cards",
-        href: "/cards",
-      },
-      {
-        title: "Diagrams",
-        href: "/diagrams",
-      },
-      {
-        title: "Filetree",
-        href: "/filetree",
-      },
-      {
-        title: "Lists",
-        href: "/lists",
-      },
-      {
-        title: "Maths",
-        href: "/maths",
-      },
-      {
-        title: "Notes",
-        href: "/notes",
-      },
-      {
-        title: "Steps",
-        href: "/steps",
-      },
-      {
-        title: "Table",
-        href: "/table",
-      },
-      {
-        title: "Tabs",
-        href: "/tabs",
-      },
-    ],
-  },
+  }
+  // {
+  //   heading: "Introduction",
+  //   title: "Basic Setup",
+  //   href: "/basic-setup",
+  //   items: [
+  //     {
+  //       title: "Installation",
+  //       href: "/installation",
+  //     },
+  //     {
+  //       title: "Setup",
+  //       href: "/setup",
+  //     },
+  //     {
+  //       title: "Changelog",
+  //       href: "/changelog",
+  //     },
+  //   ],
+  // },
+  // {
+  //   spacer: true,
+  // },
+  // {
+  //   title: "Navigation",
+  //   href: "/navigation",
+  //   heading: "Documents",
+  // },
+  // {
+  //   title: "Structure",
+  //   href: "/structure",
+  //   items: [
+  //     {
+  //       title: "Deep",
+  //       href: "/deep",
+  //       items: [
+  //         {
+  //           title: "Deeper",
+  //           href: "/deeper",
+  //           items: [
+  //             {
+  //               title: "Even deeper",
+  //               href: "/even-deeper",
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   spacer: true,
+  // },
+  // {
+  //   title: "Markdown",
+  //   href: "/markdown",
+  //   heading: "Components",
+  //   items: [
+  //     {
+  //       title: "Cards",
+  //       href: "/cards",
+  //     },
+  //     {
+  //       title: "Diagrams",
+  //       href: "/diagrams",
+  //     },
+  //     {
+  //       title: "Filetree",
+  //       href: "/filetree",
+  //     },
+  //     {
+  //       title: "Lists",
+  //       href: "/lists",
+  //     },
+  //     {
+  //       title: "Maths",
+  //       href: "/maths",
+  //     },
+  //     {
+  //       title: "Notes",
+  //       href: "/notes",
+  //     },
+  //     {
+  //       title: "Steps",
+  //       href: "/steps",
+  //     },
+  //     {
+  //       title: "Table",
+  //       href: "/table",
+  //     },
+  //     {
+  //       title: "Tabs",
+  //       href: "/tabs",
+  //     },
+  //   ],
+  // },
 ]
